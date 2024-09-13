21c1cce47eba45aef9c20068579fdd31642357be
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-13 15:49:50 +0100

    COMMENT LEARNING

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const fs = require('fs')
+// // const fs = require("fs");
+// const http = require("http");
+// const url = require("url");
+// fs.readFileSync();
 
-fs.readFileSync()
+// const server = http.createServer((req, res) => {
+//   const pathName = req.url;
+
+//   if (pathName === "/overview") {
+//     res.end("the is the OVERVIEW");
+//   } else if (pathName === "/product") {
+//     res.end("the is the PRODUCT");
+//   }
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("Listening on port 8000");
+// });
+
