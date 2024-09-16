50f70175aa30052040659edf489c68e0e2c099b3
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-16 14:34:19 +0100

    Added more functions

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 
 const port = process.env.PORT || 3000;
 // console.log(app.get("env"));
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((con) => {
+  console.log(con.connections);
+  console.log("DB connection established");
+});
 
 // Start Server
 app.listen(port, () => {
