da1ad18930cfc3e928a30b1f94466d43d645e41b
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-23 00:40:20 +0100

    Started refactoring

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,12 @@ import {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours
 } from "../controllers/tourController.js";
 
 const router = express.Router();
 
+router.route('/top-5-cheap').get(aliasTopTours, getTours)
 // router.param("id", checkID);
 
 router.route("/").get(getTours).post(createTour);
